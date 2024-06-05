e493ff100a5b1d26b33fb0fbab5065430e58ede8
FarhanGits <daffafarhan05@gmail.com>
2024-06-06 02:01:11 +0700

    add auth route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const route = require('express').Router();
 const batikRoute = require('./BatikController');
-const predictRoute = require('./PredictController')
+const predictRoute = require('./PredictController');
+const authRoute = require('./AuthController');
 
 route.use('/batik', batikRoute);
 route.use('/predict', predictRoute);
+route.use('/auth', authRoute);
 
 module.exports = route;
